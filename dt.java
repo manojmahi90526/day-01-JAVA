245e6436967a5babf3c2076fdde4a6280f9fea85
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-02 01:13:53 -0800

    Add files via upload

---
+++ dt.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class dt{
+public static void main(String args[]){
+    int i=29;
+    char c='M';
+    float f=7.29f;
+    double d=729;
+    System.out.println(i);
+    System.out.println(c);
+    System.out.println(f);
+    System.out.println(d);
+}
+}
