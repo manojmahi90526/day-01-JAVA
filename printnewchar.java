e24411b087a15831ba0be095c68f1eb1a2c8dbc0
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-02 01:46:42 -0800

    Add files via upload

---
+++ printnewchar.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class nc
+{
+    public static void main(String args[])
+    {
+      char c='A';
+      int b=c;
+      b+=1;
+      char v=(char)b;
+      System.out.println(v);
+      
+
+    }
+}
